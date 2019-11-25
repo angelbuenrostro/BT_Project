dc867ccec9713860184615bae017a9a92d209c52
Angel Buenrostro <angel.buenrostro.cs@gmail.com>
2019-11-24 20:51:56 -0500

    ADD: Custom View Cell + Autolayout constraints

--- Pokedex/Pokedex/ViewControllers/PokemonTableViewController.swift
+++ Pokedex/Pokedex/ViewControllers/PokemonTableViewController.swift
@@ -26,15 +26,13 @@ class PokemonTableViewController: UITableViewController {
         return 0
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokeCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
---
+++ Pokedex/Pokedex/ViewControllers/Views/PokemonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonTableViewCell.swift
+//  Pokedex
+//
+//  Created by Angel Buenrostro on 11/24/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
