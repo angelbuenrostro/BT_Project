89eb9087fb98fda40585bf96a23586fc74731fdf
Angel Buenrostro <angel.buenrostro.cs@gmail.com>
2019-11-24 20:29:17 -0500

    Create: New Application

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Angel Buenrostro on 11/24/19.
+//  Copyright © 2019 Angel Buenrostro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
